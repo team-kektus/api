4f8f6bebc6b29bc50bf94d273ed2ff89d7124b15
Markus Peterson <marcus17777@gmail.com>
2017-03-25 16:52:20 +0200

    Capistrano configuration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,6 @@ set :repo_url, "git@github.com:team-kektus/api.git"
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
 
-set :deploy_to, "/home/kektus/api"
-
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -40,7 +38,7 @@ set :pty, true
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.2.6'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby}
 set :rbenv_roles, :all # default value
 
