ba85581cfd779548f6e015e9f618e9a6163749a1
Markus Peterson <marcus17777@gmail.com>
2017-03-25 16:37:38 +0200

    Capistrano configuration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ set :pty, true
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.2.6'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby}
 set :rbenv_roles, :all # default value
 
 set :user,            'kektus'
