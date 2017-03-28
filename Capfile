c5986f7f5cd5e60e7035d00d6bfb0e51633d291f
Markus Peterson <marcus17777@gmail.com>
2017-03-28 19:32:51 +0300

    Capistrano configuration.

--- Capfile
+++ Capfile
@@ -28,15 +28,15 @@ install_plugin Capistrano::SCM::Git
 #   https://github.com/capistrano/passenger
 #
 # require "capistrano/rvm"
-require "capistrano/rbenv"
+# require "capistrano/rbenv"
 # require "capistrano/chruby"
-require "capistrano/bundler"
+# require "capistrano/bundler"
 # require "capistrano/rails/assets"
 # require "capistrano/rails/migrations"
 # require "capistrano/passenger"
 require "capistrano/puma"
 
-install_plugin Capistrano::Puma  # Default puma tasks
+# install_plugin Capistrano::Puma  # Default puma tasks
 # install_plugin Capistrano::Puma::Workers  # if you want to control the workers (in cluster mode)
 # install_plugin Capistrano::Puma::Jungle # if you need the jungle tasks
 # install_plugin Capistrano::Puma::Monit  # if you need the monit tasks
--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,7 @@ set :puma_workers,    1
 # Don't change these unless you know what you're doing
 set :use_sudo,        false
 set :stage,           :development
-set :deploy_via,      :remote_cache
+# set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -74,17 +74,17 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_dirs,  %w{tmp/pids tmp/cache tmp/sockets}
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
+# namespace :puma do
+#   desc 'Create Directories for Puma Pids and Socket'
+#   task :make_dirs do
+#     on roles(:app) do
+#       execute "mkdir #{shared_path}/tmp/sockets -p"
+#       execute "mkdir #{shared_path}/tmp/pids -p"
+#     end
+#   end
+#
+#   before :start, :make_dirs
+# end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
